6483c099b5e90dbc7e75fb93177e7e5d803c1872
Logan Yamamoto Mac <lkyamamo@usc.edu>
2025-11-15 02:55:21 -0800

    taking away initial printing of all timesteps that is done in process_bonds_inline_3atom_mpi_optimized.py

--- calculations/lammps/new-dielectric-constant/process_bonds_inline_3atom_mpi_optimized.py
+++ calculations/lammps/new-dielectric-constant/process_bonds_inline_3atom_mpi_optimized.py
@@ -428,8 +428,6 @@ def get_timesteps_to_process(start, end, increment, output_file):
     
     # Calculate which timesteps NEED processing
     timesteps_to_process = sorted(all_timesteps - already_processed)
-
-    print(f"Timesteps to process: {timesteps_to_process}", flush=True)
     
     return timesteps_to_process, already_processed
 
